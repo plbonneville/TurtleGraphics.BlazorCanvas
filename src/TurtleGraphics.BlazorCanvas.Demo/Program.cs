656fab215080b7f69c0f5a1de3573a035e6be48c
Pier-Luc Bonneville <9940262+plbonneville@users.noreply.github.com>
2020-12-06 14:44:02 -0500

    Added files to the solution

--- src/TurtleGraphics.BlazorCanvas.Demo/Program.cs
+++ src/TurtleGraphics.BlazorCanvas.Demo/Program.cs
@@ -10,18 +10,18 @@ using Microsoft.Extensions.Logging;
 
 namespace TurtleGraphics.BlazorCanvas.Demo
 {
-public class Program
-{
-    public static async Task Main(string[] args)
+    public class Program
     {
-        var builder = WebAssemblyHostBuilder.CreateDefault(args);
-        builder.RootComponents.Add<App>("#app");
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
 
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-        builder.Services.AddScoped<TurtleJsInterop>();
+            builder.Services.AddScoped<TurtleJsInterop>();
 
-        await builder.Build().RunAsync();
+            await builder.Build().RunAsync();
+        }
     }
-}
 }
